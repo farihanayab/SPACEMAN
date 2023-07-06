df052ff2e82bf56873192b64494abb35b92b555a
farihanayab <nayabf12@gmail.com>
2023-07-06 09:11:38 -0400

    main.js

--- main.js
+++ main.js
@@ -18,6 +18,12 @@ var categories = {
     TVshows: []
 
   };
+
+
+var guessedLetters = [];
+
+  // object[key] = value
+  // categories['foods'] --> [array...]
   
   // Function to display available categories
   function displayCategories() {
@@ -26,84 +32,85 @@ var categories = {
       console.log(category);
     }
   }
-  
+  function handleUserSelection(category) {
   // Function to handle category selection
-  function handleCategorySelection(category) {
-    var words = categories[category];
-    if (!words) {
-      console.log("Invalid category. Please choose a valid category.");
-      return;
-    }
-  
+
     // Randomly select a word from the chosen category
-    var randomIndex = Math.floor(Math.random() * words.length);
-    var selectedWord = words[randomIndex];
+    var randomIndex = Math.floor(Math.random() * category.length);
+    var selectedWord = category[randomIndex];
     console.log("Selected word:", selectedWord);
   
     // Start the Hangman game with the selected word
     startSpaceman(selectedWord);
   }
-  
+  // 
+  function handlePlayerGuess(letter) {
+    // Check if the letter has already been guessed
+    console.log('We are now in handlePlayerGuess')
+    if (guessedLetters.includes(letter)) {
+      console.log("You already guessed that letter. Try again.");
+      return;
+    }
+
+    // Add the letter to the guessed letters array
+    guessedLetters.push(letter);
+
+    // Check if the letter is present in the word
+    if (word.includes(letter)) {
+      console.log("Correct guess!");
+      // You can perform any additional actions or logic here
+    } else {
+      remainingAttempts--;
+      console.log("Incorrect guess! Remaining attempts: " + remainingAttempts);
+      // You can perform any additional actions or logic here
+    }
+
+    // Check if the game is over
+    if (remainingAttempts === 0) {
+      console.log("Game over! You ran out of attempts.");
+      // You can perform any additional actions or logic here
+    }
+  }
   // Function to start the Hangman game
   function startSpaceman(word) {
     var remainingAttempts = 6; // Maximum attempts allowed
-  
+    console.log("We are now in startSpaceman function")
+    // start getting guesses or letters from the player 
     // Function to handle player's guess
-    function handlePlayerGuess(letter) {
-      // Check if the letter has already been guessed
-      if (guessedLetters.includes(letter)) {
-        console.log("You already guessed that letter. Try again.");
-        return;
-      }
+
+    // write logic to get a letter from the player.
+    // hard code (delete later)
+    let letter = 'a'
+    function handlePlayerGuess(letter)
+  }
+
+
   
-      // Add the letter to the guessed letters array
-      guessedLetters.push(letter);
-  
-      // Check if the letter is present in the word
-      if (word.includes(letter)) {
-        console.log("Correct guess!");
-        // You can perform any additional actions or logic here
-      } else {
-        remainingAttempts--;
-        console.log("Incorrect guess! Remaining attempts: " + remainingAttempts);
-        // You can perform any additional actions or logic here
-      }
-  
-      // Check if the game is over
-      if (remainingAttempts === 0) {
-        console.log("Game over! You ran out of attempts.");
-        // You can perform any additional actions or logic here
-      }
-    }
+// Play Game
+// show the player the categoriess
+displayCategories();
+// have the player pick a category
+
+// let category = prompt("what category do you wan to pick?");
+// console.log("The Player has selected " + category)
+
+let category = 'foods';
+
+console.log("The player has selected " + category + "!")
+console.log("Game Begin")
+// get the input from the client (look up how to accept input from client and save it to a variable);
+// set the input to variable called userSelection = "";
+// call handleCategoryFunction(userSelection);
+console.log(categories[category])
+handleUserSelection(categories[category]);
 
 
 
 
 
 
-// have a var that counts how many incorrect guesses
-// Initialize the incorrectGuesses variable
-var incorrectGuesses = 0;
-
-// Function to handle an incorrect guess
-function handleIncorrectGuess() {
-  incorrectGuesses++; // Increment the count of incorrect guesses
-  console.log("Incorrect guess! Total incorrect guesses: " + incorrectGuesses);
-  // You can perform any additional actions or logic here
-}
-
-
-// have an array that stores the guess of the player: 
-// Initialize the array to store player's guesses
-var playerGuesses = [];
-
-// Function to handle a player's guess
-function handlePlayerGuess(guess) {
-  playerGuesses.push(guess); // Add the guess to the array
-  console.log("Player's guesses: " + playerGuesses);
-  // You can perform any additional actions or logic here
-}
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// garbage code
 // have a var that stores the current word
     // word = 'computer
 
@@ -114,7 +121,20 @@ function handlePlayerGuess(guess) {
 
 // if the player guess correctly they win (put in winning number)
 // else they lose
+// game logic 
 
-  
-let guessedWord = '_ _ _ _ _'
-let guesseWord = '_ P P _ _ '// (object or array) 
+// have a var that counts how many incorrect guesses
+// Initialize the incorrectGuesses variable
+// var incorrectGuesses = 0;
+
+// // Function to handle an incorrect guess
+// function handleIncorrectGuess() {
+//   incorrectGuesses++; // Increment the count of incorrect guesses
+//   console.log("Incorrect guess! Total incorrect guesses: " + incorrectGuesses);
+//   // You can perform any additional actions or logic here
+// }
+
+
+// have an array that stores the guess of the player: 
+// Initialize the array to store player's guesses
+// var playerGuesses = [];
