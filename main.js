f4225acbe8385a37e0c3052e83733c8818fef1d7
farihanayab <nayabf12@gmail.com>
2023-07-08 01:32:13 -0400

    Add eventlistener to letter

--- main.js
+++ main.js
@@ -11,174 +11,141 @@
 
 // GLOBAL VARIABLES
 // Categories and words
-var categories = {
-  foods: ["apple", "pizza", "cake"],
-  countries: ["mexico", "china", "canada"],
-  animals: ["kangaroo", "elephant", "shark"],
-};
+// var categories = {
+//   foods: ["apple", "pizza", "cake"],
+//   countries: ["mexico", "china", "canada"],
+//   animals: ["kangaroo", "elephant", "shark"],
+// };
+// ------------------------ variables ----------------------------------- // 
+
+// words we are using
 const wordsArr = ["APPLE", "PIZZA", "CAKE"];
-let currentWord = wordsArr[Math.floor((Math.random() * (wordsArr.length)))];
-console.log(currentWord);
-let guessedLetter;
-let tries = 9;
-// Letters a person has guessed
-let guessedLetters = [];
+// current word the user is guessing 
+let currentWord;
+let underscores;
+// guessed letters by user
+let guessedLetters;
 // Maximum attempts allowed
-let remainingAttempts = 6;
-// current word
-const aEl = document.getElementById("A");
-const bEl = document.getElementById("B");
+let remainingAttempts;
+
+
+// ------------------------- DOM Elements I Need ---------------------------- // 
+
+const playButton = document.getElementById('play')
+const resetButton = document.getElementById('reset')
 const wordEl = document.getElementById("word");
 const messageEl = document.getElementById("message");
-
-let underscores = [];
-let currentWordArr = [...currentWord];
-console.log(currentWordArr);
-currentWordArr.forEach((element) => {
-  underscores.push("_");
-  wordEl.innerText = underscores.join(" ");
-});
-
-// show the player the categoriess
-displayCategories();
+var letterEl = document.getElementsByClassName("letter");
 
 
-// ----------------------------------clicks--------------------------------------------------------
 
-aEl.addEventListener("click", function (event) {
-  guessedLetter = event.target.id;
-  tries--;
-  if (tries <= 0) {
-    messageEl.innerText = "gameover";
-  } else {
-    if (currentWord.includes(guessedLetter) && !underscores.includes("_")) {
-      messageEl.innerText = "You Win";
-      let index = currentWord.indexOf(event.target.id);
-      underscores[index] = event.target.id;
-      wordEl.innerText = underscores.join(" ");
-    } else if (currentWord.includes(guessedLetter)) {
-      let index = currentWord.indexOf(event.target.id);
-      underscores[index] = event.target.id;
-      wordEl.innerText = underscores.join(" ");
-    } else {
-      messageEl.innerText = "This letter is not in the word";
-    }
-  }
-});
-bEl.addEventListener("click", function (event) {
-  guessedLetter = event.target.id;
-  tries--;
-  if (tries <= 0) {
-    messageEl.innerText = "gameover";
-  } else {
-    if (currentWord.includes(guessedLetter) && !underscores.includes("_")) {
-      messageEl.innerText = "You Win";
-      let index = currentWord.indexOf(event.target.id);
-      underscores[index] = event.target.id;
-      wordEl.innerText = underscores.join(" ");
-    } else if (currentWord.includes(guessedLetter)) {
-      let index = currentWord.indexOf(event.target.id);
-      underscores[index] = event.target.id;
-      wordEl.innerText = underscores.join(" ");
-    } else {
-      messageEl.innerText = "This letter is not in the word";
-    }
-  }
-});
+// ----------------------------Functions to Start the Game ------------------------- // 
 
-
-// Step1: Allow the player to pick a category
-// Function to display available categories
-function displayCategories() {
-  console.log("Available categories:");
-  for (let category in categories) {
-    console.log(category);
-  }
-  // WRITE CODE that grabs the category via DOM from the users selection *******
-  // pass this category to handleUserSelction(category)*****
-  function handleUserSelection(categories) {}
+// display the current word as dashes on the screen
+function wordToDash(wordArr) {
+     wordArr.forEach((element) => {
+        underscores.push("_");
+        wordEl.innerText = underscores.join(" "); 
+    });  
 }
 
-// Step2 Pick a random word from categories depending on the user selection
-function handleUserSelection(category) {
-  // Randomly select a word from the chosen category
-  let randomIndex = Math.floor(Math.random() * category.length);
-  let selectedWord = category[randomIndex];
-  console.log("Selected word:", selectedWord);
-  // Start the Hangman game with the selected word
-  startSpaceman(selectedWord);
+// start the game with init function
+function init() {
+    console.log('calling init function now')
+    underscores = [];
+    currentWord = wordsArr[Math.floor((Math.random() * (wordsArr.length)))];
+    let currentWordArr = [...currentWord];
+    console.log(currentWordArr)
+    let guessedLetters = [];
+    remainingLives = 6;
+    wordToDash(currentWordArr);
 }
 
-// Step3 Start the spaceman game using the word the computer chose for the player
-function startSpaceman(word) {
-  currentWord = word;
-  console.log("We are now in startSpaceman function");
-  // WRITE ******
-  // START THE GAME BY ADDING THE BLANK LETTERS TO THE PAGE SO USER CAN TRY GUESSING THE WORD
-  // function or something that counts how many letters are in the word, and for each letter is adds a blank line onto the page
-  // design this so that when a user guesses a letter correctly, that letter appears on the screen
-  // create a dive for each letter and give it a unique id with that letter id='guess-id'
-  // when a player guesses correctly, that div can go from "dispaly:none" to "display:block" and the letter will appear on screen
-  // note: look up how to add letters to a div
-  // <div class='hide-letter' id='guess-A'>"A"</div>
+ // playButton.addEventListener("click",init);
 
-  // ALLOW USER TO GUESS LETTERS
-  // let the user click a ABC letter ******
-  // WRITE get the value of selected letter -> save it to a variable -> pass this var to handlePlayerGuess(var)*****
-  // make sure the letter could be grabbed via DOM let letter = '', using handleClick
-  let letter = "";
-  function handlePlayerGuess(letter) {}
+// START THE GAME
+init()
+
+// ------------------------------------------------------------------------------------------
+
+// Allow the User to Click Letters and Handle These Clicks
+
+const myLetters = document.getElementsByClassName("letter");
+
+function letterClicked(e) {
+  console.log(e.target.id);  // Get ID of Clicked Element
+    // disable the letter 
+    // check if this letter exists inside the currentWordArr 
+    // if it exists --> it means they guessed right
+        // add this to the dash at the correct spot
+    // else they guessed wrong 
+        // minus a life
+        // add a body part of space man to the page
 }
-// --------------------------messages------------------------------------------------------
-// Step4 Check if the players guess is correct or incorrect
-// if it is correct - add the letter to the line
-// if it wrong - take away a point from remaining guesses and add a body part for spaceman
-function handlePlayerGuess(letter) {
-  // Check if the letter has already been guessed
-  console.log("We are now in handlePlayerGuess");
-  if (guessedLetters.includes(letter)) {
-    // display a message to the user telling them that they already guessed this letter. ******
-    console.log("You already guessed that letter. Try again.");
-    return;
-  }
 
-  // Add the letter to the guessed letters array
-  // create a box in html to store your guesses *****
-  // add this letter to that box ***
-  // OR after a player guesses a letter, disable or make that letter disapear ex:
-  // document.getElementById("A").style.display="none";
-  // document.getElementById("mainFrameTwo").style.display="block";
-  guessedLetters.push(letter);
-  // Check if the letter is present in the word
-  if (word.includes(letter)) {
-    console.log("Correct guess!");
-    // You can perform any additional actions or logic here
-    // add the correct guess to the line (where the word is)***
-  } else {
-    remainingAttempts--;
-    console.log("Incorrect guess! Remaining attempts: " + remainingAttempts);
-    // You can perform any additional actions or logic here
-    // LOGIC TO MAKE SPACEMAN BODY PARTS APPEAR ONE BY ONE
-  }
-
-  // Check if the game is over
-  if (remainingAttempts === 0) {
-    console.log("Game over! You ran out of attempts.");
-    return;
-    // You can perform any additional actions or logic here
-  } else {
-    // ask the player for another guess
-    startSpaceMan(currentWord);
-  }
+for (let letter of myLetters) {
+  letter.addEventListener("click", letterClicked);
 }
-// Function to start the Hangman game
-
-//
-// Play Game
-
-// reference
-subMenuEL.addEventListener("click", function (guesses) {});
 
 
+// document.getElementById("A").addEventListener("click", handleClick);
+// function handleClick(event) {
+//     // get the id of the letter so we know which letter is clicked
+//     console.log(event.target.innerHTML);
+    
+// }
 
+// aEl.addEventListener("click", function(event) {
+//   guessedLetter = event.target.id;
+//   tries--;
+//   if (tries <= 0) {
+//     messageEl.innerText = "gameover";
+//   } else {
+//     if (currentWord.includes(guessedLetter) && !underscores.includes("_")) {
+//       messageEl.innerText = "You Win";
+//       let index = currentWord.indexOf(event.target.id);
+//       underscores[index] = event.target.id;
+//       wordEl.innerText = underscores.join(" ");
+//     } else if (currentWord.includes(guessedLetter)) {
+//       let index = currentWord.indexOf(event.target.id);
+//       underscores[index] = event.target.id;
+//       wordEl.innerText = underscores.join(" ");
+//     } else {
+//       messageEl.innerText = "This letter is not in the word";
+//     }
+//   }
+// });
+// bEl.addEventListener("click", function(event) {
+//   guessedLetter = event.target.id;
+//   tries--;
+//   if (tries <= 0) {
+//     messageEl.innerText = "gameover";
+//   } else {
+//     if (currentWord.includes(guessedLetter) && !underscores.includes("_")) {
+//       messageEl.innerText = "You Win";
+//       let index = currentWord.indexOf(event.target.id);
+//       underscores[index] = event.target.id;
+//       wordEl.innerText = underscores.join(" ");
+//     } else if (currentWord.includes(guessedLetter)) {
+//       let index = currentWord.indexOf(event.target.id);
+//       underscores[index] = event.target.id;
+//       wordEl.innerText = underscores.join(" ");
+//     } else {
+//       messageEl.innerText = "This letter is not in the word";
+//     }
+//   }
+// });
 
+// // --------------------------messages------------------------------------------------------
+
+// function handlePlayerGuess(letter) {
+//   // Check if the letter has already been guessed
+//   console.log("We are now in handlePlayerGuess");
+//   if (guessedLetters.includes(letter)) {
+//     // display a message to the user telling them that they already guessed this letter. ******
+//     console.log("You already guessed that letter. Try again.");
+//     return;
+//   }
+// }
+
+//     subMenuEL.addEventListener("click", function(guesses) { });
