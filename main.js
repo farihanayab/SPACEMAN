826476b831f6a0b116150a940a8394b5fe3f22d1
farihanayab <nayabf12@gmail.com>
2023-07-10 03:10:20 -0400

    Add remaining lives display

--- main.js
+++ main.js
@@ -3,47 +3,19 @@
 // words we are using
 const wordsArr = [
   "ADORABLE",
-  "ADORED",
-  "ADVANCED",
   "AFRAID",
   "AGED",
-  "AGGRAVATING",
   "AGGRESSIVE",
   "AGILE",
-  "AGITATED",
-  "AGONIZING",
   "AGREEABLE",
   "AJAR",
   "ALARMED",
-  "ALARMING",
   "ALERT",
-  "ALIENATED",
   "ALIVE",
-  "ALL",
-  "APPREHENSIVE",
-  "APPROPRIATE",
-  "APT",
-  "ARCTIC",
-  "ARID",
-  "AROMATIC",
-  "ARTISTIC",
-  "ASHAMED",
-  "ASSURED",
-  "ASTONISHING",
-  "ATHLETIC",
-  "ATTACHED",
-  "ATTENTIVE",
-  "ATTRACTIVE",
-  "AUSTERE",
-  "AUTHENTIC",
-  "AUTHORIZED",
-  "AUTOMATIC",
-  "AVARICIOUS",
   "AVERAGE",
   "AWARE",
   "AWESOME",
   "AWFUL",
-  
 ];
 // current word the user is guessing
 let currentWord;
@@ -64,118 +36,92 @@ const messageEl = document.getElementById("message");
 const letterEl = document.getElementsByClassName("letter");
 const myLetters = document.getElementsByClassName("letter");
 
-// ----------------------------Functions to Start the Game ------------------------- // 
+// ----------------------------Functions to Start the Game ------------------------- //
 
 for (let letter of myLetters) {
   letter.addEventListener("click", letterClicked);
-};
-
+}
 
 // display the current word as dashes on the screen
 function wordToDash(wordArr) {
   wordArr.forEach((element) => {
-      underscores.push("_");
-      wordEl.innerText = underscores.join(" ");
+    underscores.push("_");
+    wordEl.innerText = underscores.join(" ");
   });
 }
 
 // start the game with init function
 function init() {
-  playButton.innerHTML = 'Reset'
-  console.log('calling init function now')
-  messageEl.innerText = 'Use the alphabet in the textbox below to guess a letter'
+  playButton.innerHTML = "Reset";
+  messageEl.innerText =
+    "Use the alphabet in the textbox below to guess a letter";
   underscores = [];
-  currentWord = wordsArr[Math.floor((Math.random() * (wordsArr.length)))];
+  currentWord = wordsArr[Math.floor(Math.random() * wordsArr.length)];
   currentWordArr = [...currentWord];
-  console.log(currentWordArr)
   let guessedLetters = [];
   remainingLives = 6;
+  document.getElementById(
+    "guess-remaining"
+  ).innerHTML = `Guesses Remaining = ${remainingLives}`;
   wordToDash(currentWordArr);
-  let showWord = document.getElementById('word').classList.remove('hidden');
+  let showWord = document.getElementById("word").classList.remove("hidden");
+  let showGuesses = document
+    .getElementById("guess-remaining")
+    .classList.remove("hidden");
 }
 
 function resetGame() {
-  messageEl.innerText = 'Use the alphabet in the textbox below to guess a letter';
-  init()
+  messageEl.innerText =
+    "Use the alphabet in the textbox below to guess a letter";
+  init();
 }
 
 // when a player clicks player the game should start
 
-playButton.addEventListener('click', init);
+playButton.addEventListener("click", init);
 
 // ---------------------------------Allow the User to Click Letters and Handle These Clicks---------------------------------------------------------
 
-function endGame() {
-  // disable all the characters
-  allLetters = document.querySelectorAll('letter');
-  console.log(allLetters);
-  console.log('Game is Over!')
-  // ask the player if they want to play again?
-  // reset evrything
-}
-
 function letterClicked(event) {
-
   guessedLetter = event.target.id;
   if (guessedLetters.includes(guessedLetter)) {
-      console.log('You already guessed this letter!')
-      messageEl.innerText = 'You already guessed this letter!'
+    messageEl.innerText = "You already guessed this letter!";
   } else {
-      guessedLetters.push(guessedLetter)
-      // disable the letter 
-      // document.getElementById(`${guessedLetter}`).classList.add('disable');
+    guessedLetters.push(guessedLetter);
   }
-  
+
   if (remainingLives <= 0) {
-      messageEl.innerText = "gameover";
-      document.getElementById(`${bodyPart}`).classList.remove('hidden');
-      // let allRemainingLetters = document.getElementsByClassName('letter');
-      let remainingLetters = document.querySelectorAll('.letter')
-      playButton.innerHTML = 'Play Again'
-      for (l in remainingLetters) {
-          console.log(l)
-      }
+    messageEl.innerText = "You lose, Gameover";
+    playButton.innerHTML = "Play Again";
   } else {
-      if (currentWord.includes(guessedLetter)) {
-          var condition = true;
-          for (;condition;) {
-              if (!currentWordArr.includes(guessedLetter)) {
-                  condition = false;
-              } else {
-                  let index = currentWordArr.indexOf(event.target.id);
-                  currentWordArr.splice(index, 1, '_');
-                  console.log(currentWordArr);
-                  underscores[index] = event.target.id;
-                  wordEl.innerText = underscores.join(" ");
-                  console.log('underscores: ' + underscores)
-                   if (!underscores.includes("_")) {
-                      console.log("You win!");
-                      // check if everything has been guessed right
-                      messageEl.innerText = "You win!";
-                      endGame();
-              }
-          } 
+    if (currentWord.includes(guessedLetter)) {
+      var condition = true;
+      for (; condition; ) {
+        if (!currentWordArr.includes(guessedLetter)) {
+          condition = false;
+        } else {
+          let index = currentWordArr.indexOf(event.target.id);
+          currentWordArr.splice(index, 1, "_");
+
+          underscores[index] = event.target.id;
+          wordEl.innerText = underscores.join(" ");
+
+          if (!underscores.includes("_")) {
+            // check if everything has been guessed right
+            messageEl.innerText = "You win!";
+          }
+        }
       }
-      } else {
-              messageEl.innerHTML = "This letter is not in the word";
-              remainingLives--;
-              // show a body part;
-              console.log('remainingLives' + remainingLives)
-              bodyPart = remainingLives;
-              // remove the hidden class from the body part so it shows up
-              document.getElementById(`${bodyPart}`).classList.remove('hidden')
-       
-}}} 
+    } else {
+      messageEl.innerHTML = "This letter is not in the word";
+      remainingLives--;
+      // show a body part;
 
-
-  // add an event listener to every letter so I can handle player clicks
-
-
-
-// function resetButtons() {
-//     let disabledLetters = document.querySelectorAll('disable');
-//     for (i = 0; i < disabledLetters.length; i++) {
-//         console.log(disabledLetters[i])
-//         disabledLetters[i].classList.remove('disable')       
-//     }
-// }
+      bodyPart = remainingLives;
+      // Change the remaining lives number on the screen
+      document.getElementById(
+        "guess-remaining"
+      ).innerHTML = `Guesses Remaining = ${remainingLives}`;
+    }
+  }
+}
